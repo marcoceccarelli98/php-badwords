b08e6df80600264ce4e022c809a75e0903ce080e
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-06-17 19:50:28 +0200

    [Bonus]

--- censure.php
+++ censure.php
@@ -3,7 +3,10 @@
 $censureWord = $_GET['censureWord'];
 $paragraph = $_GET['paragraph'];
 
-$censParagraph = str_replace($censureWord,"***",$paragraph);
+$parLength = strlen($paragraph);
+
+$censPar = str_replace($censureWord,"***",$paragraph);
+$censParLength = strlen($censPar);
 
 ?>
 
@@ -15,12 +18,23 @@ $censParagraph = str_replace($censureWord,"***",$paragraph);
     <title>Parola Censurata</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
  </head>
- <body class="vh-100 d-flex">
-    <div class="text-center m-auto">
-        <h1 class="mb-5">Testo Censurato</h1>
-        <p>
-            <?php echo $censParagraph ?>
+ <body class="vh-100 d-flex bg-primary">
+    <!-- ORIGINAL TEXT -->
+    <div class="text-center m-auto bg-white p-4 rounded">
+        <h1 class="mb-5">Testo Originale</h1>
+        <p class="fs-2 mb-5">
+            <?php echo $paragraph ?>
         </p>
+        <span class="fs-5 fw-bold">Lunghezza: <?php echo $parLength ?></span>
+    </div>
+
+    <!-- CENSORED TEXT -->
+    <div class="text-center m-auto bg-white p-4 rounded">
+        <h1 class="mb-5">Testo Censurato</h1>
+        <p class="fs-2 mb-5">
+            <?php echo $censPar ?>
+        </p>
+        <span class="fs-5 fw-bold">Lunghezza: <?php echo $censParLength ?></span>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
--- index.php
+++ index.php
@@ -4,20 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Test</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
-    <form action="censure.php" method="GET">
-        <div>
-            <label for="censureWord">Parola da Censurare</label>
+<body class="vh-100 d-flex bg-primary">
+    <form action="censure.php" method="GET" class="text-center m-auto bg-white p-4 rounded">
+        <div class="text-center">
+            <label class="d-block mb-1" for="censureWord">Parola da Censurare</label>
             <input type="text" name="censureWord" id="censureWord">
         </div>
-        <div>
-            <label for="paragraph">Paragrafo</label>
-            <input type="text" name="paragraph" id="paragraph">
+        <div class="my-3 text-center">
+            <label class="d-block mb-1" for="paragraph">Paragrafo</label>
+            <input style="height:400px;" type="text" name="paragraph" id="paragraph">
         </div>
         <div>
             <button>Invia</button>
         </div>
     </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
